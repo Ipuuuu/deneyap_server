adeb96b0b4fc4f1d1f987ae7f571ce0d22a1f4b6
Ipuuuu <evangelynne.04@gmail.com>
2025-07-12 19:23:02 +0300

    added control for motors endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,47 @@ const char * password = "cezerilab2024";
 // const char * ssid = "GAMZELICANSU20";
 // const char * password = "BOLU5678";
 
+//motor pins
+const uint8_t rightForward = A0;
+const uint8_t rightBackward = A1;
+const uint8_t leftForward = A2;
+const uint8_t leftBackward = A3;
+
+const uint8_t leftSpeed = D14;
+const uint8_t RightSpeed = D13;
 
 WebServer server(80);
 
+struct Control{
+  uint8_t left_speed;
+  uint8_t right_speed;
+  String direction;
+};
+
+Control control ; 
 
 const uint32_t BLINK_INTERVAL = 200;
 
 void handleStatus() ;
 void setRoutes();
+void back_and_forth(uint8_t l = 200, uint8_t r = 200);
+void forward(uint8_t l = 200, uint8_t r = 200);
+void backward(uint8_t l = 200, uint8_t r = 200);
+void stop();
+void turn_left(uint8_t l = 150, uint8_t r = 255);
+void turn_right(uint8_t l = 255, uint8_t r = 150);
+void handleControl();
+
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(LED, OUTPUT);
+
+  pinMode(rightForward, OUTPUT);
+  pinMode(rightBackward, OUTPUT);
+  pinMode(leftForward, OUTPUT);
+  pinMode(leftBackward, OUTPUT);
 
   //Connect to wifi
   WiFi.begin(ssid, password);
@@ -48,6 +76,7 @@ void setup() {
   Serial.println("Available endpoints:");
   Serial.println("  GET  /status     - Get system status");
   Serial.println("  GET  /led     - Post led commands");
+  Serial.println("  GET  /control     - Post motor commands");
 
 }
 
@@ -88,7 +117,7 @@ void handleLed(){
 
   if (server.hasArg("plain")) {
     String state = server.arg("plain");
-    Serial.println("Received raw JSON: " + state);
+    Serial.println("LED Command received: " + state);
     
     StaticJsonDocument<100> resdoc;
     DeserializationError error = deserializeJson(resdoc, state);
@@ -134,6 +163,64 @@ void handleLed(){
   }
 }
 
+void handleControl(){
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+
+  if(server.hasArg("plain")){
+    String body = server.arg("plain");
+    Serial.println("Control Commanded received: " + body);
+  }
+
+  StaticJsonDocument<200> doc;
+  DeserializationError error = deserializeJson(doc, server.arg("plain"));
+
+  if (error) {
+    server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+    return;
+  }
+
+  if (doc.containsKey("direction")) {
+    control.direction = doc["direction"].as<String>();
+    Serial.println("Direction: " + control.direction);
+  }
+
+  if (doc.containsKey("right_speed") && doc.containsKey("left_speed")) {
+    control.left_speed = doc["left_speed"].as<uint8_t>();
+    control.right_speed = doc["right_speed"].as<uint8_t>();
+    Serial.println("Left Speed: " + String(control.left_speed));
+    Serial.println("Right Speed: " + String(control.right_speed));
+  }
+
+  if(control.direction == "forward") {
+    forward(control.left_speed, control.right_speed);
+    server.send(200, "application/json", "{\"status\":\"Moving forward\"}");
+  }
+  else if(control.direction == "backward") {
+    backward(control.left_speed, control.right_speed);
+    server.send(200, "application/json", "{\"status\":\"Moving backward\"}");
+  }
+  else if(control.direction == "left") {
+    turn_left(control.left_speed, control.right_speed);
+    server.send(200, "application/json", "{\"status\":\"Turning left\"}");
+  }
+  else if(control.direction == "right") {
+    turn_right(control.left_speed, control.right_speed);
+    server.send(200, "application/json", "{\"status\":\"Turning right\"}");
+  }
+  else if(control.direction == "stop") {
+    stop();
+    server.send(200, "application/json", "{\"status\":\"Stopped\"}");
+  }
+  else if(control.direction == "back_and_forth") {
+    back_and_forth(control.left_speed, control.right_speed);
+    server.send(200, "application/json", "{\"status\":\"Back and forth\"}");
+  }
+  else {
+    server.send(400, "application/json", "{\"error\":\"Unknown direction\"}");
+  }
+
+}
+
 void setRoutes(){
   server.onNotFound([]() {
     server.sendHeader("Access-Control-Allow-Origin", "*");
@@ -147,6 +234,79 @@ void setRoutes(){
   server.on("/status", HTTP_GET, handleStatus);
 
   server.on("/led", HTTP_POST, handleLed);
+
+  server.on("/control", HTTP_POST, handleControl);
   
 }
 
+
+void back_and_forth(uint8_t l, uint8_t r){
+  analogWrite(leftSpeed, l);
+  analogWrite(RightSpeed, r);
+
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+
+  delay(2000);
+
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, HIGH);
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
+
+  delay(2000);
+
+}
+
+void forward(uint8_t l, uint8_t r){
+  analogWrite(leftSpeed, l);
+  analogWrite(RightSpeed, r);
+
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+
+}
+
+void backward(uint8_t l , uint8_t r ){
+  analogWrite(leftSpeed, l);
+  analogWrite(RightSpeed, r);
+
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, HIGH);
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
+}
+
+void stop(){
+  analogWrite(leftSpeed, 0);
+  analogWrite(RightSpeed, 0);
+
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, LOW);
+}
+
+void turn_left(uint8_t l, uint8_t r){
+  analogWrite(leftSpeed, l);
+  analogWrite(RightSpeed, r); 
+
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
+}
+
+void turn_right(uint8_t l, uint8_t r){
+  analogWrite(leftSpeed, l);
+  analogWrite(RightSpeed, r);
+
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, HIGH);
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+}
